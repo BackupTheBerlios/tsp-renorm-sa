e13a935244144e0ab975b9e7cedd3a04327021d8
stereo <stereo>
2007-11-27 19:29:01 +0000

    Distance is computed correctly

---
+++ trunk/src/distance.c
@@ -0,0 +1,38 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "tsp.h"
+#include "distance.h"
+
+double 
+route_length(const Tsp *tsp, const int *route, int num_cities)
+{
+	assert(num_cities != 0);
+	assert(route != NULL);
+	assert(tsp != NULL);
+	assert(tsp->distance_type == EUC_2D);
+
+	double length;
+	double height;
+	double route_lngth = 0;
+	City *city_curr, *city_nxt;
+
+	for (unsigned int i = 0; i < (num_cities - 1); i++) {
+		assert(route[i] < tsp->dimension);
+	
+		/* Temporary assign the cities to readable variables. */
+		city_curr = &tsp->cities[route[i]];
+		city_nxt = &tsp->cities[route[i + 1]];
+		
+		/* Cities should be different. */
+		assert(city_curr != city_nxt);
+
+		/* Use Pythagoras law to compute the distance. */
+		length = city_curr->x - city_nxt->x;
+		height = city_curr->y - city_nxt->y;
+		route_lngth += sqrt(length * length + height * height);
+	}
+
+	return route_lngth;
+}
---
+++ trunk/src/distance.h
@@ -0,0 +1,6 @@
+#ifndef DISTANCE_H
+#define DISTANCE_H
+
+double route_length(const Tsp *tsp, const int *route, int num_cities);
+
+#endif
