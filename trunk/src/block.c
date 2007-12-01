6d78acc0ba310b830227f42cd08bd63a74365a41
stereo <stereo>
2007-12-01 21:29:24 +0000

    The routines are tested and work. It is now possible to use routines to save city and block information to be read in R. Using R a plot can be made (see comments in block.h)

--- trunk/src/block.c
+++ trunk/src/block.c
@@ -3,14 +3,15 @@
 #include <stdlib.h>
 #include <sysexits.h>
 #include <assert.h>
+#include <stdio.h>
 
 #include "block.h"
 #include "tsp.h"
 
 double extern rotation = 0;
 
-
-static double _rotation = FP_INFINITE;
+/* This contains all the cached values. */
+static double _rotation = FP_NAN;
 static double _x_max;
 static double _x_min;
 static double _y_max;
@@ -21,7 +22,8 @@ static unsigned int _num_cities;
 static void check_limits(void);
 static void rotate(void);
 
-grd *create_grd(const unsigned int *length, const unsigned int *height)
+grd *
+create_grd(const unsigned int *length, const unsigned int *height)
 {
 	/* Update the cache containing the rotated cities if necessary. */
 	rotate();
@@ -29,8 +31,11 @@ grd *create_grd(const unsigned int *length, const unsigned int *height)
 	/* Create a new grid and initialize all the values. */
 	grd *new_grd;
 
-	if ((new_grd = malloc(sizeof(grd))) == NULL)
+	if ((new_grd = calloc(1, sizeof(grd))) == NULL)
 		errx(EX_OSERR, "Not enough memory!");
+
+	new_grd->length = *length;
+	new_grd->height = *height;
 
 	if ((new_grd->block = calloc(*length, sizeof(int *))) == NULL)
 		errx(EX_OSERR, "Not enough memory!");
@@ -42,19 +47,17 @@ grd *create_grd(const unsigned int *length, const unsigned int *height)
 			new_grd->block[i][j] = NO_CITY;
 	}
 
-	new_grd->length = *length;
-	new_grd->height = *height;
-
+	
 	/* Compute the necessary values which will be used to index the cities. */
-	double x_step = fabs(_x_min - _x_max) / (double)*length;
-	double y_step = fabs(_y_min - _y_max) / (double)*height;
+	double x_step = fabs(_x_min - _x_max) / (double)(*length - 1);
+	double y_step = fabs(_y_min - _y_max) / (double)(*height - 1);
 
 	/* Determine for each city in which block it should be added. If multiple
 	 * cities are in one block, set the block to contain MANY_CITIES otherwise
 	 * set the block value to the city number. */
 	for (int i = 0; i < _num_cities; i++) {
 		unsigned int x = rint(floor((_rot_cities[i].x - _x_min) / x_step));
-		unsigned int y = rint(floor((_rot_cities[i].y - _y_min) / x_step));
+		unsigned int y = rint(floor((_rot_cities[i].y - _y_min) / y_step));
 		if (!has_city(new_grd->block[x][y]))
 			new_grd->block[x][y] = i;
 		else
@@ -64,7 +67,14 @@ grd *create_grd(const unsigned int *length, const unsigned int *height)
 	return new_grd;
 }
 
-
+void
+print_cities(FILE *f)
+{
+	assert(f != NULL);
+	(void)fprintf(f, "city_x city_y\n");
+	for (int i = 0; i < _num_cities; i++)
+		(void)fprintf(f, "%lf %lf\n", _rot_cities[i].x, _rot_cities[i].y);
+}
 
 void 
 free_grd(grd *grid)
@@ -83,7 +93,7 @@ static void
 rotate(void)
 {
 	/* It the old rotation is the same nothing has to be done. */
-	if (_rotation == rotation)
+	if (_rotation == rotation) 
 		return;
 
 	/* Free and allocate memory for the cities in the rotated plane. */
@@ -100,10 +110,80 @@ rotate(void)
 		_rot_cities[i].y = -tsp->cities[i].x * sin(rotation) + 
 			tsp->cities[i].y * cos(rotation);
 	}
+	/* Update the cached cities. */
 	_rotation = rotation;
 	_num_cities = tsp->dimension;
 
 	check_limits();
+}
+
+void
+print_grd_lines(grd *grid, FILE *f)
+{
+	assert(grid != NULL);
+	assert(f != NULL);
+	/* Compute the necessary values which will be used to index the cities. */
+	double x_step = fabs(_x_min - _x_max) / (double)(grid->length - 1);
+	double y_step = fabs(_y_min - _y_max) / (double)(grid->height - 1);
+
+	/* Print the grid. At the end of each line a NA is needed to halt the line.
+	 * See for more information on this the R manual for lines(). */
+	/* The header. */
+	(void)fprintf(f, "x y\n");
+	/* First the vertical lines. */
+	for (double x = _x_min; x < _x_max; x += x_step) {
+		(void)fprintf(f, "%lf %lf\n", x, _y_min);
+		(void)fprintf(f, "%lf %lf\n", x, _y_max);
+		(void)fprintf(f, "%lf NA\n");
+	}
+	(void)fprintf(f, "%lf %lf\n", _x_max, _y_min);
+	(void)fprintf(f, "%lf %lf\n", _x_max, _y_max);
+	(void)fprintf(f, "%lf NA\n");
+
+	/* The horizontal lines. */
+	for (double y = _y_min; y < _y_max; y += y_step) {
+		(void)fprintf(f, "%lf %lf\n", _x_min, y);
+		(void)fprintf(f, "%lf %lf\n", _x_max, y);
+		(void)fprintf(f, "%lf NA\n");
+	}
+	(void)fprintf(f, "%lf %lf\n", _x_min, _y_max);
+	(void)fprintf(f, "%lf %lf\n", _x_max, _y_max);
+	(void)fprintf(f, "%lf NA\n");
+}
+
+void
+print_grd_points(grd *grid, FILE *f)
+{
+	assert(grid != NULL);
+	assert(f != NULL);
+	/* Compute the necessary values which will be used to index the cities. */
+	double x_step = fabs(_x_min - _x_max) / (double)(grid->length - 1);
+	double y_step = fabs(_y_min - _y_max) / (double)(grid->height - 1);
+	
+	/* Print the header. */
+	(void)fprintf(f, "x y pch\n");
+	unsigned int x_i = 0;
+	unsigned int y_i;
+	for (double x = _x_min + 0.5 * x_step; x < _x_max; x += x_step) {
+		y_i = 0;
+		for (double y = _y_min + 0.5 * y_step; y < _y_max; y += y_step) {
+			/* Print the coordinate of the center of a box. */
+			(void)fprintf(f, "%lf %lf", x, y);
+			/* Determine the logo which will represent the center of the box. 
+			 * These numbers are R codes. */
+			if (has_cities(grid->block[x_i][y_i]))
+				/* pch=24 is a triangle point up. */
+				(void)fprintf(f, " 24\n");
+			else if (has_city(grid->block[x_i][y_i]))
+				/* pch=19 is a solid circle. */
+				(void)fprintf(f, " 19\n");
+			else
+				/* pch=23 is a diamond. */
+				(void)fprintf(f, " 23\n");
+			y_i++;
+		}
+		x_i++;
+	}
 }
 
 static void
@@ -112,20 +192,28 @@ check_limits(void)
 	if (_rot_cities == NULL)
 		return;
 
+	/* Cache the x-max and min and the y-max and min. */
 	_x_max = -FP_INFINITE;
-	_y_max = -FP_INFINITE;
 	_x_min = FP_INFINITE;
+	_y_max = -FP_INFINITE;
 	_y_min = FP_INFINITE;
 
 	for (int i = 0; i < _num_cities; i++) {
-		if (_rot_cities[i].x > _x_max)
+		if (_rot_cities[i].x > _x_max) 
 			_x_max = _rot_cities[i].x;
-		if (_rot_cities[i].y > _y_max)
-			_y_max = _rot_cities[i].y;
 		if (_rot_cities[i].x < _x_min)
 			_x_min = _rot_cities[i].x;
-		if (_rot_cities[i].y > _y_min)
+		if (_rot_cities[i].y > _y_max)
+			_y_max = _rot_cities[i].y;
+		if (_rot_cities[i].y < _y_min)
 			_y_min = _rot_cities[i].y;
 	}
+
+	/* Built some margin to be sure that all the cities are included in 
+	 * a box. */
+	_x_max += X_MARGIN;
+	_x_min -= X_MARGIN;
+	_y_max += Y_MARGIN;
+	_y_min -= Y_MARGIN;
 }
 
--- trunk/src/block.h
+++ trunk/src/block.h
@@ -5,15 +5,15 @@
 
 #include "tsp.h"
 
-#define Y_OFFSET 0.001
-#define X_OFFSET Y_OFFSET
+#define X_MARGIN 0.05
+#define Y_MARGIN X_MARGIN
 
 /* Define the value which is returned when no city is in the block. */
 #define NO_CITY -1
 #define MANY_CITIES -2
 /* Determine if the returned value is a city. */
 #define has_city(x) ((x) != NO_CITY)
-#define has_cities(x) ((x) != MANY_CITIES)
+#define has_cities(x) ((x) == MANY_CITIES)
 
 
 /* Sets the rotation of the blocks. */
@@ -23,10 +23,40 @@ typedef struct {
 	int **block;
 	unsigned int length;
 	unsigned int height;
+	double x_marg;
+	double y_marg;
 } grd;
 
 /* */
 grd *create_grd(const unsigned int *length, const unsigned int *height);
 void free_grd(grd *grid);
+
+/*
+ * The following two functions can be used to print the boxes which represent 
+ * the grid. The functions save all the data in a space seperated format with
+ * a header. These files can be read with R (see cran.r-project.org) and 
+ * plotted. Using commands like this:
+ *
+ * # Read the cities saved with the function print_cities()
+ * cities<-read.table("cities",header=T)
+ * # Plot the cities.
+ * plot(cities$city_x, cities$city_y,axes=F)
+ * 
+ * # Read the lines which represent the grid. This file is saved 
+ * # using print_grd_lines()
+ * lines<-read.table("lines",header=T)
+ * # Plot the lines.
+ * lines(as.vector(lines))
+ *
+ * # Read the center dots of each box. If no cities are in the box the point
+ * # is a diamond, if there is one city is a solid circle. In the
+ * # case there are multiple cities the centre is a triangle with point up.
+ * points<-read.table("points",header=T)
+ * # Plot the points.
+ * points(points$x, points$y, pch=points$pch)
+ */
+void print_grd_lines(grd *grid, FILE *f);
+void print_grd_points(grd *grid, FILE *f);
+void print_cities(FILE *f);
 
 #endif /* BLOCK_H */
