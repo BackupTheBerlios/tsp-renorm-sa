f1f63766f5212d701b67de28e8a8932b3151c90c
stereo <stereo>
2007-12-02 12:49:47 +0000

    Added path files which make it possible to export a path to R. NOT TESTED!

---
+++ trunk/src/path.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <assert.h>
+
+#include "path.h"
+#include "tsp.h"
+
+void 
+print_path(unsigned int *path, FILE *f)
+{
+	assert(path != NULL);
+	assert(f != NULL);
+
+	(void)sprintf(f, "x y\n");
+	for (int i = 0; i < tsp->dimension; i++) 
+		(void)sprintf(f, "%lf %lf\n", tsp->cities[path[i]].x, 
+				tsp->cities[path[i]].y);
+}
+
+
---
+++ trunk/src/path.h
@@ -0,0 +1,6 @@
+#ifndef PATH_H
+#define PATH_H
+#include <stdio.h>
+
+void print_path(unsigned int *path, FILE *f);
+#endif
